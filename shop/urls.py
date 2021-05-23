6b1c0e00ba85b634605f9200e610aefddc8aec13
siavash <rahimi.f.h@gmil.com>
2021-05-23 14:10:51 +0430

    wholsaler search

--- shop/urls.py
+++ shop/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('about/',views.About_us, name='aboutus'),
     path('contact/',views.contact_us, name='contactus'),
     path('search_result/',views.searchb_us, name='searchb'),
+    path('search_r/',views.searchwho_us, name='searwho'),
     path('<slug:category_slug>/', views.product_list,name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail,name='product_detail'),
 ]
